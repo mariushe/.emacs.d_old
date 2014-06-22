5debd2fcba76f65bb4ad107a8d83c6989f17c7ee
Marius Herring <marius@medmgmt-12.tajen.edu.tw>
2014-06-22 22:59:10 +0200

    first emacs settings

---
+++ elpa/epl-0.7/epl-autoloads.el
@@ -0,0 +1,18 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21413 62196 825153
+;;;;;;  0))
+
+;;;***
+
+(provide 'epl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epl-autoloads.el ends here
